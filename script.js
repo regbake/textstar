de96489abbb48371138e833cadd11dfdb3842192
regbake <regbake@gmail.com>
2018-02-02 13:20:08 -0800

    added node server

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//this is the script to make the things happen...
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.listen(3000, function(){
+  console.log("Hooked up to port 3k");
+});
+
+module.exports = app; 
