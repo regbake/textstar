182a680c2ac0448d846eac0b43475dc46fb7eb6c
regbake <regbake@gmail.com>
2018-02-03 15:21:12 -0800

    hooking up the Function module

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
+const TextFunctions = require("./scripts/TextFunctions.js").test;
 
 //multer shiz
 var multer = require("multer");
@@ -13,6 +14,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", function(req, res){
+  TextFunctions(); 
   res.render("index", {
     files: []
   });
